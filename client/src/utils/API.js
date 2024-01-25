92e91cac8d909370815f644d7830c602262c0ac7
Isaiah <ikapr600@gmail.com>
2024-01-25 18:01:20 -0500

    nav and login ch, jsx

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,5 @@
 // route to get logged in user's info (needs the token)
-export const getMe = (token) => {
+export const getMe = async (token) => {
   return fetch('/api/users/me', {
     headers: {
       'Content-Type': 'application/json',
